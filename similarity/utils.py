475330ce7faf1bf1292ae65c8b2ffd952b7976cc
yashchitre03 <41328573+yashchitre03@users.noreply.github.com>
2021-03-12 16:14:33 -0600

    Add ReadOnly descriptor

--- similarity/utils.py
+++ similarity/utils.py
@@ -1,5 +1,4 @@
-class Preprocessor:
-    punctuations = set(r'''!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~''')
+class ReadOnly:
 
     def __set_name__(self, owner, name):
         self.public_name = name
@@ -9,24 +8,52 @@ class Preprocessor:
         return getattr(instance, self.private_name)
 
     def __set__(self, instance, value):
-        text = value.lower()
-        clean_text = ''.join(filter(lambda c: c not in self.punctuations, text))
-        words = clean_text.split()
-        filtered_words = list(filter(lambda word: word.isalpha(), words))
-
-        setattr(instance, self.private_name, filtered_words)
+        raise AttributeError('read-only attribute')
 
 
 class SimilarityCalculator:
-    t1 = Preprocessor()
-    t2 = Preprocessor()
+    punctuations = set(r'''!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~''')
+    text1, text2 = ReadOnly(), ReadOnly()
+    v1, v2 = ReadOnly(), ReadOnly()
 
-    def __init__(self, t1, t2):
-        self.t1 = t1
-        self.t2 = t2
+    def __init__(self, text1, text2):
+        self._text1, self._text2 = text1, text2
+        self._v1, self._v2 = self.to_vector()
+
+    def to_vector(self):
+        data1, data2 = self.clean_samples()
+        vocab = tuple(set(data1 + data2))
+
+        v1 = list(map(lambda word: data1.count(word), vocab))
+        v2 = list(map(lambda word: data2.count(word), vocab))
+
+        return v1, v2
+
+    def clean_samples(self):
+        def clean(value):
+            text = value.lower()
+            clean_text = ''.join(filter(lambda c: c not in self.punctuations, text))
+            words = clean_text.split()
+            filtered_words = list(filter(lambda word: word.isalpha(), words))
+            return filtered_words
+
+        return clean(self.text1), clean(self.text2)
+
+    @staticmethod
+    def dot(x, y):
+        res = 0
+        for x_elem, y_elem in zip(x, y):
+            res += x_elem * y_elem
+
+        return res
+
+    def manhattan(self):
+        pass
+
+    def euclidean(self):
+        pass
 
     def jaccard(self):
-        print(self.t1, self.t2)
         t1_unique = set(self.t1)
         t2_unique = set(self.t2)
         t1_intersect_t2 = len(t1_unique & t2_unique)
@@ -40,24 +67,20 @@ class SimilarityCalculator:
         return round(score, 4)
 
     def cosine(self):
-        vocab = tuple(set(self.t1 + self.t2))
-        t1_unique = set(self.t1)
-        t2_unique = set(self.t2)
-
-        v1 = list(map(lambda word: 1 if word in t1_unique else 0, vocab))
-        v2 = list(map(lambda word: 1 if word in t2_unique else 0, vocab))
+        v1, v2 = self.to_vector()
 
         try:
-            score = self.dot(v1, v2) / (self.dot(v1, v1) * self.dot(v2, v2))**0.5
+            score = self.dot(v1, v2) / (self.dot(v1, v1) * self.dot(v2, v2)) ** 0.5
         except ZeroDivisionError:
             score = 0
 
         return round(score, 4)
 
-    @staticmethod
-    def dot(x, y):
-        res = 0
-        for x_elem, y_elem in zip(x, y):
-            res += x_elem * y_elem
+    def dice(self):
+        pass
 
-        return res
+    def matching(self):
+        pass
+
+    def overlap(self):
+        pass
